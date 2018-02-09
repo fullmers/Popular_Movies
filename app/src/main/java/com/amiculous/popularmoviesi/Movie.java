ed26ab7b7638311949d5a2672becd8815ef7bc7f
fullmers <sarah.fullmer@gmail.com>
2018-02-09 10:00:40 +0100

    define Movie object based on json structure from moviedb API

---
+++ app/src/main/java/com/amiculous/popularmoviesi/Movie.java
@@ -0,0 +1,97 @@
+package com.amiculous.popularmoviesi;
+
+/**
+ * Created by sarah on 09/02/2018.
+ */
+
+public class Movie {
+    
+    private int voteCount;
+    private int id;
+    private boolean isVideo;
+    private int voteAverage;
+    private String title;
+    private double popularity;
+    private String posterPath;
+    private String originalLanguage;
+    private String originalTitle;
+    private int[] genreIds;
+    private String backdropPath;
+    private boolean isAdult;
+    private String overview;
+    private String releaseDate;
+
+    public Movie(int voteCount, int id, boolean isVideo, int voteAverage, String title, double popularity, String posterPath, String originalLanguage, String originalTitle, int[] genreIds, String backdropPath, boolean isAdult, String overview, String releaseDate) {
+        this.voteCount = voteCount;
+        this.id = id;
+        this.isVideo = isVideo;
+        this.voteAverage = voteAverage;
+        this.title = title;
+        this.popularity = popularity;
+        this.posterPath = posterPath;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.genreIds = genreIds;
+        this.backdropPath = backdropPath;
+        this.isAdult = isAdult;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isVideo() {
+        return isVideo;
+    }
+
+    public int getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public int[] getGenreIds() {
+        return genreIds;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+}
---
+++ app/src/main/java/com/amiculous/popularmoviesi/utils/JsonUtils.java
@@ -0,0 +1,8 @@
+package com.amiculous.popularmoviesi.utils;
+
+/**
+ * Created by sarah on 09/02/2018.
+ */
+
+public class JsonUtils {
+}
