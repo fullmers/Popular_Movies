7f4b21377b7da66eaa9f4fd01b906f6be1cfe6b4
fullmers <sarah.fullmer@gmail.com>
2018-02-10 09:02:38 +0100

    start using butterknife

--- app/src/main/java/com/amiculous/popularmoviesi/MovieDetailActivity.java
+++ app/src/main/java/com/amiculous/popularmoviesi/MovieDetailActivity.java
@@ -3,20 +3,28 @@ package com.amiculous.popularmoviesi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
     Movie mMovie;
     private static final String TAG = MovieDetailActivity.class.getSimpleName();
 
+    @BindView(R.id.text_movie_title) TextView TvMovieTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        ButterKnife.bind(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             mMovie = (Movie) extras.getParcelable(getString(R.string.movie_extra_key));
+            TvMovieTitle.setText(mMovie.getTitle());
             Log.d(TAG,mMovie.getTitle());
         }
     }
